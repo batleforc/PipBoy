8c4969d9f02de8330af1bee5540b78c8864eee5b
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-09 21:02:06 +0100

    [add] Mise en place des log

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -6,10 +6,16 @@ import (
 	helper "pipboy/Helper"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "[${remote_ip} : ${time_rfc3339_nano}] ${status} : ${method} => ${uri}\n",
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
