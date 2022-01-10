817cd87a611677ea2d015e19b66ada7fb3fc2cde
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-10 15:24:58 +0100

    [add] OIDC AXA + React router dom + fake routing

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
+  const Composant = ({ value }) => <h1>{value}</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link style={{ margin: 1 }} to="/">
+          HOME
+        </Link>
+        <Link style={{ margin: 1 }} to="/1">
+          1
+        </Link>
+        <Link style={{ margin: 1 }} to="/2">
+          2
+        </Link>
+        <Link style={{ margin: 1 }} to="/3">
+          3
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Composant value="HOME" />} />
+        <Route path="/1" element={<Composant value="1" />} />
+        <Route path="/2" element={<Composant value="2" />} />
+        <Route path="/3" element={<Composant value="3" />} />
+      </Routes>
     </div>
   );
 }
