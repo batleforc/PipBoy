2d179a5a31713fce959314b09e5e74b58a7d802f
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-11 17:54:09 +0100

    [add] Create Helper auth

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	helper "pipboy/Helper"
-	"strings"
 
 	"github.com/Nerzal/gocloak/v10"
 	"github.com/labstack/echo/v4"
@@ -24,25 +22,17 @@ func main() {
 	}))
 
 	e.GET("/", func(c echo.Context) error {
-		externalToken := c.Request().Header.Get("Authorization")
-		client := gocloak.NewClient(helper.GetStringEnv("OIDC_HOSTNAME", "https://auth.weebo.fr"))
-		ctx := context.Background()
-		externalToken = strings.Replace(externalToken, "Bearer", "", -1)
-		externalToken = strings.Trim(externalToken, " ")
-		clientId := helper.GetStringEnv("OIDC_CLIENTID", "pipboy")
-		clientSecret := helper.GetStringEnv("OIDC_CLIENTSECRET", "5a161e19-2d93-4860-9927-06097a824814")
-		clientRealm := helper.GetStringEnv("OIDC_REALM", "MasterKluster")
-		rptToken, err := client.RetrospectToken(ctx, externalToken, clientId, clientSecret, clientRealm)
+		externalToken := helper.ParseTokenHeader(c.Request().Header.Get("Authorization"))
+		rptToken, err := helper.GetRetrospectToken(externalToken)
 		if err != nil {
 			fmt.Printf("An error has been encountered, %s", err.Error())
 		}
-		//fmt.Print(rptToken.Active)
 		fmt.Print(rptToken.String())
 		type Retour struct {
 			Truc bool `json:"name"`
 			rpt  gocloak.RetrospecTokenResult
 		}
-		return c.JSON(http.StatusOK, Retour{Truc: !*rptToken.Active, rpt: *rptToken})
+		return c.JSON(http.StatusOK, Retour{Truc: !*rptToken.Active, rpt: rptToken})
 	})
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", helper.GetStringEnv("PORT", "3001"))))
 }
