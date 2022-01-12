4a266b1e314c151f27bea366efc948b87c78e1fa
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-12 20:28:08 +0100

    [add] GetNamespaceName

--- BackEnd/Helper/parse.go
+++ BackEnd/Helper/parse.go
@@ -1,8 +1,19 @@
 package helper
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Nerzal/gocloak/v10"
+)
 
 func ParseTokenHeader(token string) string {
 	token = strings.Replace(token, "Bearer", "", -1)
 	return strings.Trim(token, " ")
+}
+
+func GetNamespaceName(userInfo gocloak.UserInfo) string {
+	firstName := *userInfo.GivenName
+	lastName := *userInfo.FamilyName
+	return fmt.Sprintf("%s%s%s", GetStringEnv("NAMESPACE_PREFIX", "mk-"), firstName[0:1], lastName)
 }
