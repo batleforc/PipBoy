0685ce20e19071eb07c9864b7e58fe2285a14b7f
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-13 11:15:44 +0100

    [add] Minimal profile information

--- FrontEnd/src/Component/Profile.js
+++ FrontEnd/src/Component/Profile.js
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { AuthenticationContext } from "@axa-fr/react-oidc-context";
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Profile = () => {
-  return <h1>Profile user</h1>;
+  const [data, setData] = useState();
+  return (
+    <AuthenticationContext.Consumer>
+      {(props) => {
+        if (props.oidcUser && data == undefined)
+          axios
+            .get("http://localhost:3001", {
+              headers: {
+                Authorization: `Bearer ${props.oidcUser.access_token}`,
+              },
+            })
+            .then((data) => data.data)
+            .then((data) => setData(data));
+        console.log(data, props.oidcUser);
+        return (
+          <>
+            {props.oidcUser && data != undefined ? (
+              <div>
+                <p>Namespace Exist : {data.NamespaceExist.toString()}</p>
+                <p>Namespace : {data.NamespaceName}</p>
+                <p>User : {props.oidcUser.profile.name}</p>
+                <p>Username : {props.oidcUser.profile.preferred_username}</p>
+              </div>
+            ) : (
+              <p>
+                You shouldn't be here GO BACK <Link to="/">HOME</Link>
+              </p>
+            )}
+          </>
+        );
+      }}
+    </AuthenticationContext.Consumer>
+  );
 };
 
 export default Profile;
--- FrontEnd/src/Component/User.js
+++ FrontEnd/src/Component/User.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { AuthenticationContext } from "@axa-fr/react-oidc-context";
-import axios from "axios";
 const User = () => {
   return (
     <AuthenticationContext.Consumer>
       {(props) => {
-        console.log(props);
-        if (props.oidcUser)
-          axios.get("http://localhost:3001", {
-            headers: { Authorization: `Bearer ${props.oidcUser.access_token}` },
-          });
         return (
           <>
             {props.oidcUser ? (
