07bd37a6011200a24b873420046ed340e8871d66
Maxime Leriche <maxleriche.60@gmail.com>
2022-01-14 00:19:04 +0100

    [add] Documentation

--- Readme.md
+++ Readme.md
@@ -2,12 +2,14 @@
 
 Le projet PipBoy est un projet d'environnement de travails dans Kubernetes.
 
+Ce nom est temporaire et voué a changer (comme les 3/4 de mes projets)
+
 ## Infra
 
 Le projet est baser sur trois namespace kubernetes:
 
 - Auth: Qui contiendra toute la solution de gestion d'identité
-- Guac: Qui contient la partie acces VNC/SSH aux container
+- Guac: Qui contient la partie accès VNC/SSH aux container
 - PipBoy: Contiendra l'infra PipBoy (un petit toolkit que je souhaite dev)
 - UserNamespace: l'espace de dev des utilisateur (en autant d'exemplaire que nécessaire)
 
---
+++ doc/docusaurus.config.js
@@ -0,0 +1,119 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "PipBoy",
+  tagline: "Projet d'environnement de travail dans Kubernetes",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "batleforc", // Usually your GitHub org/user name.
+  projectName: "PipBoy", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/batleforc/PipBoy/tree/master/doc/docs/",
+          remarkPlugins: [
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
+          ],
+        },
+        pages: {
+          path: "src/pages",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/batleforc/PipBoy/tree/master/doc/blog",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "PipBoy",
+        logo: {
+          alt: "PipBoy",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Doc",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/batleforc/PipBoy",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Doc",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Portfolio",
+                href: "https://maxleriche.tech",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Maxweebo",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/batleforc/PipBoy",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} PipBoy, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
